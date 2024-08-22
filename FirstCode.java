aebd6c86d38759023b0d24022a96a86b968a56e5
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-08-21 20:31:20 -0700

    first program

---
+++ FirstCode.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FirstCode {
+
+        public static void printer(int n){
+      for(int i = 0; i < n; i++){
+            System.out.println("hi Guys");
+        }
+        }
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+         printer(n);
+  
+
+        sc.close();
+    }
+}
